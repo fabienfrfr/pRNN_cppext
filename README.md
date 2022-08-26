94a8ec6286c58023b5b176d9da3be11c04c6b87c
fabienfrfr <fabien.furfaro@gmail.com>
2022-08-26 19:00:39 +0200

    reverse engineering torch

--- README.md
+++ README.md
@@ -23,3 +23,13 @@ tips :
 	- use JIT to see step of code
 	- see basic autograd function (see backward)
 	- #include <iostream> for use direct library in Terminal (but need 'main') or C code (see libtorch also).
+
+## Observation JIT step :
+
+For unstacked :
+
+	input -> view -> convolution -> view -> to -> select -> to -> (loop) index -> to -> select -> to (end loop) -> [list output, index] -> cat (input Module) -> t -> addmm -> output
+
+for stacked (time dependant) :
+
+	input -> view -> convolution -> view -> (LOOP) (loop) unsqueeze (end loop) -> [list output] -> cat -> t -> addmm (END LOOP) --> [list output Module] -> cat -> output
--- torch2jit.py
+++ torch2jit.py
@@ -4,23 +4,30 @@ import torch
 from py import graph_gen, pRNN_net
 
 # parameter
-I,O = 64,16
+I,O = 16,8
 BATCH = 25
 
 graph = graph_gen.GRAPH((I,O))
+while len(graph.LIST_C) > 32 or len(graph.NEURON_LIST) < 3 :
+	graph = graph_gen.GRAPH((I,O))
+print("Generate small network for example")
+
 net = graph.NEURON_LIST#.tolist()
 
 # to trace
 model = pRNN_net.pRNN(net, BATCH, I, torch.device('cpu'))
+model_stacked = pRNN_net.pRNN(net, BATCH, I, torch.device('cpu'), STACK=True)
 
 # An example input you would normally provide to your model's forward() method.
 example = torch.randn(BATCH,I, requires_grad=True)
 
 # Use torch.jit.trace to generate a torch.jit.ScriptModule via tracing.
-output = traced_script_module = torch.jit.trace(model, example)
+traced_script_module = torch.jit.trace(model, example)
+traced_script_module_stacked = torch.jit.trace(model_stacked, example)
 
 # save
 traced_script_module.save("traced_enn_model.pt")
+traced_script_module_stacked.save("traced_enn_model_stacked.pt")
 
 """
 The model trace change following input, jit it's not adapted for convert code to C++
